0a83e5281e1aca927ac8681ece7b337a0f6ad71a
gmpassos <gracilianomp@gmail.com>
2023-08-08 22:03:09 -0300

    example: set `Hello` font to `Courier New`

--- example/win32_gui_example.dart
+++ example/win32_gui_example.dart
@@ -171,7 +171,7 @@ class TextOutput extends RichEdit {
     setTextFormatted([
       TextFormatted(" -------------------------\r\n",
           color: RGB(255, 255, 255)),
-      TextFormatted(" Hello", color: RGB(0, 255, 255), faceName: 'monospace'),
+      TextFormatted(" Hello", color: RGB(0, 255, 255), faceName: 'Courier New'),
       TextFormatted(" Word! \r\n", color: RGB(0, 255, 0)),
       TextFormatted(" -------------------------\r\n",
           color: RGB(255, 255, 255)),
